032aa1038f15e22b3b220bea387dadc98b783c51
kstadnicka <kamila.kudyba93@gmail.com>
2025-07-23 16:59:29 +0200

    Initial commit.

---
+++ src/main/java/ks/githubrepositoriesapi/GithubRepositoriesApiApplication.java
@@ -0,0 +1,13 @@
+package ks.githubrepositoriesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoriesApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoriesApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ks/githubrepositoriesapi/GithubRepositoriesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ks.githubrepositoriesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubRepositoriesApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
